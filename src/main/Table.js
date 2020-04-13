36fa9daa46252fcb07946758285109c24353d609
taishi <taishi@cutput.com>
2020-04-13 21:51:37 +0800

    add total

--- src/main/Table.js
+++ src/main/Table.js
@@ -14,7 +14,7 @@ const TableHeader = () => {
     )
 }
 
-const Tabelbody = props => {
+const Tablebody = props => {
     const itemss = props.characterData.map((items, index) => {
         return (
             <tr className="able-danger" key={index}>
@@ -34,12 +34,30 @@ const Tabelbody = props => {
     return <tbody>{itemss}</tbody>
 }
 
+const Tableend = props=>{
+       let Total=0
+       const getdata=props.characterData
+       for(var i=0;i<getdata.length;i++){
+           Total=Total+getdata[i]["valuedata"]
+       }
+       return (
+        <tr className="able-danger">
+           <td>Total</td>
+           <td>-</td>
+           <td>-</td>
+           <td>{Total}</td>
+           <td>{Total*7.2}</td>
+           </tr>
+       )
+}
+
 const Table = (props) => {
     const { characterData, removeCharacter } = props
     return (
         <table className="table table-striped table-bordered">
             <TableHeader />
-            <Tabelbody characterData={characterData} removeCharacter={removeCharacter} />
+            <Tablebody characterData={characterData} removeCharacter={removeCharacter} />
+            <Tableend characterData={characterData} />
         </table>
     );
 }
